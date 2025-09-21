c21b3e20131d2f671cf246027158c9ef3befba94
PhucUET <24020274@vnu.edu.vn>
2025-09-21 19:48:27 +0700

    ex4

---
+++ AllEqual.java
@@ -0,0 +1,12 @@
+public class AllEqual{
+    public static void main(String[] args) {
+        int x = Integer.parseInt(args[0]);
+        int y = Integer.parseInt(args[1]);
+        int z = Integer.parseInt(args[2]);
+        if(x == y && x == z) {
+            System.out.println("all equal");
+        } else {
+            System.out.println("not all equal");
+        }
+    }
+}
