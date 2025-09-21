f4c562ef320a45f5225eec8fe7a7eefea4619aa9
PhucUET <24020274@vnu.edu.vn>
2025-09-21 18:46:40 +0700

    add Introduction.java

---
+++ HW1/Introduction.java
@@ -0,0 +1,5 @@
+public class Introduction {
+    public static void main(String[] args) {
+        System.out.println("DinhGiaPhuc\t24020274\tQH-2024-I/CQ-I-IT4\tPhucUET\t24020274@vnu.edu.vn\n9 bottles of beer on the wall, 9 bottles of beer.\nTake one down, pass it around,\n8 bottles of beer on the wall, 8 bottles of beer.\nTake one down, pass it around,\n...\n1 bottle of beer on the wall, 1 bottle of beer.\nTake one down, pass it around,\nNo more bottles of beer on the wall.");
+    }
+}
